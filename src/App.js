d1c19f2062a0738d029b41c5e84cfcf43bd0f588
Joshua Niederer <josh@jpniederer.com>
2017-04-14 21:42:39 -0500

    Adding React's tic-tac-toe tutorial.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import CardList from './GitHubCards/CardList';
 import CardForm from './GitHubCards/CardForm';
+import Game from './TicTacToe/Game';
 import './App.css';
 
 class App extends Component {
@@ -25,9 +26,41 @@ class App extends Component {
           <CardForm onSubmit={this.addNewCard} />
           <CardList cards={this.state.cards} />
         </div>
+        <div>
+          <Game />
+          <div id="errors" className="game-errors"></div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+window.addEventListener('mousedown', function(e) {
+  document.body.classList.add('mouse-navigation');
+  document.body.classList.remove('kbd-navigation');
+});
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode === 9) {
+    document.body.classList.add('kbd-navigation');
+    document.body.classList.remove('mouse-navigation');
+  }
+});
+window.addEventListener('click', function(e) {
+  if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
+    e.preventDefault();
+  }
+});
+window.onerror = function(message, source, line, col, error) {
+  var text = error ? error.stack || error : message + ' (at ' + source + ':' + line + ':' + col + ')';
+  //errors.textContent += text + '\n';
+  //errors.style.display = '';
+};
+console.error = (function(old) {
+  return function error() {
+    //errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
+    //errors.style.display = '';
+    old.apply(this, arguments);
+  }
+})(console.error);
